82473c82626c2060726beda7c763aa9db6872bda
Philippe ARNAL <philiparnal@gmail.com>
2022-03-26 07:02:08 +0100

    User Form

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\ConnexionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class ConnexionController extends AbstractController
 {
@@ -13,8 +15,12 @@ class ConnexionController extends AbstractController
      */
     public function index(): Response
     {
+
+        $user = new User();
+        $form = $this->createForm(ConnexionType::class, $user);
         return $this->render('connexion/index.html.twig',[
-            'nomPage' => "Connexion"
+            'nomPage' => "Connexion",
+            'form' => $form->createView()
         ]);
     }
 }
